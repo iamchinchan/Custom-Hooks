a32821d4dcf044cbacc066a8c3a2da63589ad454
Jatin Goyal <goyaljatin344@gmail.com>
2021-11-30 15:33:55 +0530

    added cnfiguration to custom hooks, in counter app

---
+++ src/customHooks/useCounter.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+
+const useCounter = (operator) => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (operator === "+") {
+        setCounter((prevCounter) => prevCounter + 1);
+      } else {
+        setCounter((prevCounter) => prevCounter - 1);
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return counter;
+};
+
+export default useCounter;
